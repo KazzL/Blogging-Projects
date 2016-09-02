f2ccaf2f182de75e3b6cdf377b46f5e02b2a6e7d
Kas Lewis <Kas.Lewis@gmail.com>
2016-09-02 16:03:19 -0400

    Power Logging MSP430: Reading 28 bytes of data to cover all needed info except for acumulated values. Added debug statments to output DEC values in an explicit format. Values with decimal values are done using divide and modulo functions.

--- Power_Logging_MSP430FR5969_M2X/Multiple_Serial_With_MCP39F511/Multiple_Serial_With_MCP39F511.ino
+++ Power_Logging_MSP430FR5969_M2X/Multiple_Serial_With_MCP39F511/Multiple_Serial_With_MCP39F511.ino
@@ -18,12 +18,12 @@
 #define ACK 0x06
 #define NACK 0x15
 #define CSFAIL 0x51
-#define debug 1
-#define debugState 1
+#define debug 0
+#define debugState 0
 #define debugVerbose 0
 #define debugStateThree 0
-#define debugHEX 1
-#define debugDEC 0
+#define debugHEX 0
+#define debugDEC 1
 
 byte requestData[] = {0xA5, 0x08, 0x41, 0x00, 0x02, 0x4E, 0x1C, 0x5A};
 int timeSlot = 300; //Lowest 200
@@ -39,9 +39,9 @@ int state = 0;
 unsigned char inChar;
 unsigned char checksum;
 unsigned int tempUInt_16;
-signed int tempSInt_16;
-unsigned long tempULong_32;
-signed long tempSLong_32;
+signed int tempInt_16;
+unsigned long tempUInt_32;
+signed long tempInt_32;
 
 int availableBytes = 0;
 
@@ -174,61 +174,70 @@ void loop() {
 			
 		case 4:
 			#if debugDEC
-			// Serial.print("\n\rNumber of Bytes - 0x");
-			// Serial.print(reveivedData[1], DEC);
+			Serial.print("\n\rNumber of Bytes - ");
+			Serial.print(reveivedData[1], DEC);
 			
-			// Serial.print("\n\rSystem Status - 0x");
-			// Serial.print(reveivedData[3], HEX);
-			// Serial.print(reveivedData[2], HEX);
+			Serial.print("\n\rSystem Status - 0x");
+			Serial.print(reveivedData[3], HEX);
+			Serial.print(reveivedData[2], HEX);
 			
-			// Serial.print("\n\rSystem Version - 0x");
-			// Serial.print(reveivedData[5], HEX);
-			// Serial.print(reveivedData[4], HEX);
+			Serial.print("\n\rSystem Version - Year-20");
+			Serial.print(((reveivedData[5] & 0xF0) >> 4), DEC);
+			Serial.print(" Month-");
+			Serial.print((reveivedData[5] & 0x0F), DEC);
+			Serial.print(" Day-");
+			Serial.print(reveivedData[4], DEC);
 			
-			// Serial.print("\n\rVoltage RMS x 10 = 0x");
-			// Serial.print(reveivedData[7], HEX);
-			// Serial.print(reveivedData[6], HEX);
+			Serial.print("\n\rVoltage RMS = ");
+			tempUInt_16 = (reveivedData[7] << 8) | reveivedData[6];
+			Serial.print((tempUInt_16 / 10), DEC);
+			Serial.print(".");
+			Serial.print((tempUInt_16 % 10), DEC);
 						
-			// Serial.print("\n\rLine Frequency x 1000 = 0x");
-			// Serial.print(reveivedData[9], HEX);
-			// Serial.print(reveivedData[8], HEX);
+			Serial.print("\n\rLine Frequency = ");
+			tempUInt_16 = (reveivedData[9] << 8) | reveivedData[8];
+			Serial.print((tempUInt_16 / 1000), DEC);
+			Serial.print(".");
+			Serial.print((tempUInt_16 % 1000), DEC);
 						
-			// Serial.print("\n\rAnalog Input Voltage = 0x");
-			// Serial.print(reveivedData[11], HEX);
-			// Serial.print(reveivedData[10], HEX);
+			Serial.print("\n\rAnalog Input Voltage = ");
+			tempUInt_16 = (reveivedData[11] << 8) | reveivedData[10];
+			Serial.print(tempUInt_16, DEC);
 						
-			// Serial.print("\n\rPower Factor / 2^(-15) = 0x");
-			// Serial.print(reveivedData[13], HEX);
-			// Serial.print(reveivedData[12], HEX);
+			Serial.print("\n\rPower Factor = ");
+			tempUInt_32 = 0x0000FFFF & ((reveivedData[13] << 8) | reveivedData[12]);
+			Serial.print("0.");
+			Serial.print(tempUInt_32 * 3051, DEC);
 			
-			// Serial.print("\n\rCurent RMS x 1000 = 0x");
-			// Serial.print(reveivedData[17], HEX);
-			// Serial.print(reveivedData[16], HEX);
-			// Serial.print(reveivedData[15], HEX);
-			// Serial.print(reveivedData[14], HEX);
+			Serial.print("\n\rCurent RMS = ");
+			tempUInt_32 = (reveivedData[17] << 24) | (reveivedData[16] << 16) | (reveivedData[15] << 8) | reveivedData[14];
+			Serial.print((tempUInt_32 / 1000), DEC);
+			Serial.print(".");
+			Serial.print((tempUInt_32 % 1000), DEC);
+
 						
-			// Serial.print("\n\rActive Power x 100 = 0x");
-			// Serial.print(reveivedData[21], HEX);
-			// Serial.print(reveivedData[20], HEX);
-			// Serial.print(reveivedData[19], HEX);
-			// Serial.print(reveivedData[18], HEX);
+			Serial.print("\n\rActive Power = ");
+			tempUInt_32 = (reveivedData[21] << 24) | (reveivedData[20] << 16) | (reveivedData[19] << 8) | reveivedData[18];
+			Serial.print((tempUInt_32 / 100), DEC);
+			Serial.print(".");
+			Serial.print((tempUInt_32 % 100), DEC);
 									
-			// Serial.print("\n\rReactive Power x 100 = 0x");
-			// Serial.print(reveivedData[25], HEX);
-			// Serial.print(reveivedData[24], HEX);
-			// Serial.print(reveivedData[23], HEX);
-			// Serial.print(reveivedData[22], HEX);
+			Serial.print("\n\rReactive Power = ");
+			tempUInt_32 = (reveivedData[25] << 24) | (reveivedData[24] << 16) | (reveivedData[23] << 8) | reveivedData[22];
+			Serial.print((tempUInt_32 / 100), DEC);
+			Serial.print(".");
+			Serial.print((tempUInt_32 % 100), DEC);
 									
-			// Serial.print("\n\rApparent Power x 100 = 0x");
-			// Serial.print(reveivedData[29], HEX);
-			// Serial.print(reveivedData[28], HEX);
-			// Serial.print(reveivedData[27], HEX);
-			// Serial.print(reveivedData[26], HEX);
+			Serial.print("\n\rApparent Power = ");
+			tempUInt_32 = (reveivedData[29] << 24) | (reveivedData[28] << 16) | (reveivedData[27] << 8) | reveivedData[26];
+			Serial.print((tempUInt_32 / 100), DEC);
+			Serial.print(".");
+			Serial.print((tempUInt_32 % 100), DEC);
 			
-			// Serial.print("\n\rChecksum Value Reveived = 0x");
-			// Serial.print(reveivedData[30], HEX);
-			// Serial.print("\n\rChecksum Value Calculated = 0x");
-			// Serial.print(checksum, HEX);
+			Serial.print("\n\rChecksum Value Reveived = 0x");
+			Serial.print(reveivedData[30], HEX);
+			Serial.print("\n\rChecksum Value Calculated = 0x");
+			Serial.println(checksum, HEX);
 			#endif
 		
 		
